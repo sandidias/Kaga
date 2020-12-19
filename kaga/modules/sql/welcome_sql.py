3309ddf10a654811a63bb14dc62a32dc6f9c905a
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-12-19 12:52:15 +0800

    Update welcome_sql.py

--- kaga/modules/sql/welcome_sql.py
+++ kaga/modules/sql/welcome_sql.py
@@ -1,8 +1,8 @@
 import threading
 from typing import Union
 
-from ubotindo.modules.helper_funcs.msg_types import Types
-from ubotindo.modules.sql import BASE, SESSION
+from kaga.modules.helper_funcs.msg_types import Types
+from kaga.modules.sql import BASE, SESSION
 from sqlalchemy import (
     BigInteger,
     Boolean,
