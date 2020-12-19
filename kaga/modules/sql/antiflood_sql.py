d45e78110720d21263a1e4daf85576061e7bc08a
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-12-19 12:40:56 +0800

    Update antiflood_sql.py

--- kaga/modules/sql/antiflood_sql.py
+++ kaga/modules/sql/antiflood_sql.py
@@ -18,7 +18,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from ubotindo.modules.sql import SESSION, BASE
+from kaga.modules.sql import SESSION, BASE
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
