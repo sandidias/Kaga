3e1bcbc6f869e0fa1c95466f07fdf8c4dca35d61
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-12-22 09:59:26 +0800

    Update chat_status.py

--- kaga/modules/helper_funcs/chat_status.py
+++ kaga/modules/helper_funcs/chat_status.py
@@ -1,19 +1,3 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from functools import wraps
 from telegram import User, Chat, ChatMember
 from telegram.error import BadRequest, Unauthorized
@@ -243,8 +227,8 @@ def dev_plus(func):
                 pass
         else:
             update.effective_message.reply_text(
-                "This is a developer restricted command."
-                " You do not have permissions to run this.")
+                "Ini adalah perintah terbatas pengembang."
+                " Anda tidak memiliki izin untuk menjalankan ini.")
 
     return is_dev_plus_func
 
@@ -269,7 +253,7 @@ def sudo_plus(func):
                 pass
         else:
             update.effective_message.reply_text(
-                "Who dis non-admin telling me what to do? You want a punch?")
+                "Siapa non-admin yang memberitahuku apa yang harus dilakukan??")
 
     return is_sudo_plus_func
 
