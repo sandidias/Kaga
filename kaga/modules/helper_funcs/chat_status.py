11452565ed71ec367330fbf7efd630b0c9916272
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-12-22 10:16:07 +0800

    Update chat_status.py

--- kaga/modules/helper_funcs/chat_status.py
+++ kaga/modules/helper_funcs/chat_status.py
@@ -7,7 +7,6 @@ from kaga import (
     DEV_USERS,
     SUDO_USERS,
     WHITELIST_USERS,
-    SUPPORT_CHAT,
     dispatcher,
 )
 from cachetools import TTLCache
@@ -30,7 +29,6 @@ def is_user_ban_protected(
         or user_id in DEV_USERS
         or user_id in SUDO_USERS
         or user_id in WHITELIST_USERS
-        or user_id in SUPPORT_CHAT
         or chat.all_members_are_administrators
         or user_id in (777000, 1087968824)
     ):
@@ -46,7 +44,6 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
         chat.type == "private"
         or user_id in DEV_USERS
         or user_id in SUDO_USERS
-        or user_id in SUPPORT_CHAT
         or user_id in (777000, 1087968824)
         or chat.all_members_are_administrators
     ):
