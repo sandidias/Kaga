7700e40f164f0d6e26f6c99d5845de56e9af3e74
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-12-22 20:29:04 +0800

    Update ping.py

--- kaga/modules/ping.py
+++ kaga/modules/ping.py
@@ -58,7 +58,7 @@ def ping_func(to_ping: List[str]) -> List[str]:
 
         pinged_site = f"<b>{each_ping}</b>"
 
-        if each_ping is "Kaizoku" or each_ping is "Kayo":
+        if each_ping == "Kaizoku" or each_ping == "Kayo":
             pinged_site = f'<a href="{sites_list[each_ping]}">{each_ping}</a>'
             ping_time = f"<code>{ping_time} (Status: {r.status_code})</code>"
 
@@ -69,19 +69,24 @@ def ping_func(to_ping: List[str]) -> List[str]:
 
 
 @typing_action
-def ping(bot: Bot, update: Update):
-    telegram_ping = ping_func(["Telegram"])[0].split(": ", 1)[1]
+def ping(update: Update, context: CallbackContext):
+    msg = update.effective_message
+
+    start_time = time.time()
+    message = msg.reply_text("Pinging...")
+    end_time = time.time()
+    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
-    reply_msg = ("PONG!!\n"
-                 "<b>Time Taken:</b> <code>{}</code>\n"
-                 "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime))
-
-    update.effective_message.reply_text(reply_msg, parse_mode=ParseMode.HTML)
-
+    message.edit_text(
+        "PONG!!\n"
+        "<b>Time Taken:</b> <code>{}</code>\n"
+        "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        parse_mode=ParseMode.HTML)
+    
 
 @typing_action
-def pingall(bot: Bot, update: Update):
+def pingall(update: Update, context: CallbackContext):
     to_ping = ["Kaizoku", "Kayo", "Telegram", "Jikan"]
     pinged_list = ping_func(to_ping)
     pinged_list.insert(2, '')
@@ -91,7 +96,8 @@ def pingall(bot: Bot, update: Update):
     reply_msg += "\n".join(pinged_list)
     reply_msg += '\n<b>Service uptime:</b> <code>{}</code>'.format(uptime)
 
-    update.effective_message.reply_text(reply_msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+    update.effective_message.reply_text(
+        reply_msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
 
 PING_HANDLER = DisableAbleCommandHandler("ping", ping, run_async=True)
 PINGALL_HANDLER = DisableAbleCommandHandler("pingall", pingall, run_async=True)
