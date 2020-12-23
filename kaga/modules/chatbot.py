6f737afbf6e70e19ec0bbfd9d313474081b125db
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-12-23 17:30:57 +0800

    Update chatbot.py

--- kaga/modules/chatbot.py
+++ kaga/modules/chatbot.py
@@ -2,7 +2,7 @@ import html
 # AI module using Intellivoid's Coffeehouse API by @TheRealPhoenix
 from time import sleep, time
 
-import Cutiepii_Robot.modules.sql.chatbot_sql as sql
+import kaga.modules.sql.chatbot_sql as sql
 from coffeehouse.api import API
 from coffeehouse.exception import CoffeeHouseError as CFError
 from coffeehouse.lydia import LydiaAI
