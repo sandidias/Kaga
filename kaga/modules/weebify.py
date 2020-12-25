d835530bbd26cb47ad4ae98ccc936adae033cf14
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-12-25 18:45:19 +0800

    Update weebify.py

--- kaga/modules/weebify.py
+++ kaga/modules/weebify.py
@@ -1,5 +1,5 @@
-from Cutiepii_Robot import dispatcher
-from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
+from kaga import dispatcher
+from kaga.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from kaga.modules.helper_funcs.alternate import typing_action
 
