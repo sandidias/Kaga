a123e6437a39096b569094bb9a16f3e054a95f7a
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-12-25 11:44:55 +0800

    Update tts.py

--- kaga/modules/tts.py
+++ kaga/modules/tts.py
@@ -12,13 +12,12 @@ from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram import ParseMode
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
-from kaga import dispatcher
+from lynda import dispatcher
 from kaga.__main__ import STATS
 from kaga.modules.disable import DisableAbleCommandHandler
 from kaga.modules.helper_funcs.extraction import extract_user
 
-def tts(update, context):
-    args = context.args
+def tts(bot: Bot, update: Update, args):
     current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
     filename = datetime.now().strftime("%d%m%y-%H%M%S%f")
     reply = " ".join(args)
@@ -36,12 +35,11 @@ def tts(update, context):
         tts.save("k.mp3")
     with open("k.mp3", "rb") as speech:
         update.message.reply_voice(speech, quote=False)
-       
-TTS_HANDLER = DisableAbleCommandHandler("tts", tts)
-
-dispatcher.add_handler(TTS_HANDLER)
 
 
-__handlers__ = [
-    TTS_HANDLER
-]
+    __help__ = """ Text to speech
+    - /tts <your text>
+    """
+    __mod_name__ = "tts"
+
+dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))
