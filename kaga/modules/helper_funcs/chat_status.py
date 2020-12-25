a0610ed81f8096e36e787bbcd2726f04d9e50dc5
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-12-25 21:04:43 +0800

    Update chat_status.py

--- kaga/modules/helper_funcs/chat_status.py
+++ kaga/modules/helper_funcs/chat_status.py
@@ -2,11 +2,11 @@ from functools import wraps
 
 from telegram import Bot, Chat, ChatMember, Update, ParseMode
 
-from kaga import dispatcher, DEL_CMDS, WHITELIST_USERS, SARDEGNA_USERS, SUPPORT_USERS, SUDO_USERS, DEV_USERS
+from kaga import dispatcher, DEL_CMDS, WHITELIST_USERS, SUPPORT_USERS, SUDO_USERS, DEV_USERS
 
 
 def is_whitelist_plus(_chat: Chat, user_id: int, _member: ChatMember = None) -> bool:
-    return any(user_id in user for user in [WHITELIST_USERS, SARDEGNA_USERS, SUPPORT_USERS, SUDO_USERS, DEV_USERS])
+    return any(user_id in user for user in [WHITELIST_USERS, SUPPORT_USERS, SUDO_USERS, DEV_USERS])
 
 
 def is_support_plus(_chat: Chat, user_id: int, _member: ChatMember = None) -> bool:
