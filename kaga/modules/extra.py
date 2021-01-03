b213151eec6497cdbfe4b53215e57cbf7fcc702d
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2021-01-03 22:03:49 +0800

    Update extra.py

--- kaga/modules/extra.py
+++ kaga/modules/extra.py
@@ -27,6 +27,59 @@ Ini akan membuat dua tombol pada satu baris, bukan satu tombol per baris.
 Ingatlah bahwa pesan Anda <b>HARUS</b> berisi teks selain hanya tombol!
 """
 
+NEKOS_HELP = """
+Commands :
+× /neko: Mengirim Gambar sumber SFW Neko Acak.
+× /feet: Mengirim Gambar Acak Anime Feet.
+× /yuri: Mengirim Gambar sumber Yuri Acak.
+× /trap: Mengirim Gambar sumber Perangkap Acak.
+× /futanari: Mengirim Gambar sumber Futanari Acak.
+× /hololewd: Mengirimkan Random Holo Lewds.
+× /lewdkemo: Mengirimkan Random Kemo Lewds.
+× /sologif: Mengirim GIF Solo Acak.
+× /cumgif: Mengirim GIF Cum Acak.
+× /erokemo: Mengirim Gambar Random Ero-Kemo.
+× /lesbian: Mengirim Gambar Sumber Les Acak.
+× /lewdk: Mengirim Random Kitsune Lewds.
+× /ngif: Mengirim GIF Neko Acak.
+× /tickle: Mengirim GIF Gelitik Acak.
+× /lewd: Mengirim Cabul Acak.
+× /feed: Mengirim GIF Pemberian Makan Acak.
+× /eroyuri: Mengirim Gambar sumber Ero-Yuri Acak.
+× /eron: Mengirim Gambar sumber Ero-Neko Acak.
+× /cum: Mengirim Gambar Cum Acak.
+× /bjgif: Mengirim GIF Pekerjaan Pukulan Acak.
+× /bj: Mengirim Gambar sumber Pekerjaan Pukulan Acak.
+× /nekonsfw: Mengirim Gambar sumber NSFW Neko Acak.
+× /solo: Mengirim GIF Neko NSFW Acak.
+× /kemonomimi: Mengirim Gambar sumber KemonoMimi Acak.
+× /avatarlewd: Mengirim Stiker Cabul Pembalas Acak.
+× /gasm: Mengirim Stiker Orgasme Acak.
+× /poke: Mengirim GIF Poke Acak.
+× /anal: Mengirim GIF Anal Acak.
+× /hentai: Mengirim Gambar sumber Hentai Acak.
+× /avatar: Mengirim Stiker Avatar Acak.
+× /erofeet: Mengirim Gambar sumber Ero-Feet Acak.
+× /holo: Mengirim Gambar sumber Random Holo.
+× /tits: Mengirim Gambar sumber Tits Acak.
+× /pussygif: Mengirim GIF Pussy Acak.
+× /holoero: Mengirim Gambar sumber Ero-Holo Acak.
+× /pussy: Mengirim Gambar sumber Pussy Acak.
+× /hentaigif: Mengirim GIF Hentai Acak.
+× /classic: Mengirim GIF Hentai Klasik Acak.
+× /kuni: Mengirim GIF Random Pussy Lick.
+× /waifu: Mengirim Stiker Waifu Acak.
+× /kiss: Mengirim GIF Ciuman Acak.
+× /femdom: Mengirim Gambar sumber Femdom Acak
+× /cuddle: Mengirim GIF Cuddle Acak.
+× /erok: Mengirim Gambar sumber Ero-Kitsune Acak.
+× /foxgirl: Mengirim Gambar sumber FoxGirl Acak.
+× /titsgif: Mengirim GIF Payudara Acak.
+× /ero: Mengirim Gambar sumber Ero Acak.
+× /smug: Mengirim GIF Sombong Acak.
+× /baka: Mengirim GIF Teriakan Baka Acak.
+× /dva: Mengirim Gambar sumber DVA Acak.
+"""
 
 def markdown_help_sender(update: Update):
     update.effective_message.reply_text(
@@ -52,14 +105,41 @@ def markdown_help(update, context):
             ]]))
         return
     markdown_help_sender(update)
+    
+    
+    
+def nekos_help_sender(update: Update):
+    update.effective_message.reply_text(
+        MARKDOWN_HELP, parse_mode=ParseMode.HTML)
+    update.effective_message.reply_text(
+        "Harap gunakan dengan bijak :D"
+    )
+    
+    
+@typing_action
+def nekos_help(update, context):
+    if update.effective_chat.type != "private":
+        update.effective_message.reply_text(
+            'Hubungi saya di PM',
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton(
+                    "Bantuan Nodul Nekos",
+                    url=f"t.me/{context.bot.username}?start=nekoshelp")
+            ]]))
+        return
+    nekos_help_sender(update)
+
 
 
 MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
+NEKOS_HELP_HANDLER = CommandHandler("nekoshelp", nekos_help)
 
 dispatcher.add_handler(MD_HELP_HANDLER)
+dispatcher.add_handler(NEKOS_HELP_HANDLER)
 
 __mod_name__ = "Extras"
 __command_list__ = ["id"]
 __handlers__ = [
     MD_HELP_HANDLER,
+    NEKOS_HELP_HANDLER,
 ]
